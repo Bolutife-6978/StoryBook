623ba7ef1c9407e0cb38e36eff7b45bfad293ef1
Ogunfowokan Boluwatife <ogunfowokansherrifdeen@gmail.com>
2021-08-15 14:55:54 +0100

    Saving before applying finishing touches

--- helpers/hbs.js
+++ helpers/hbs.js
@@ -8,7 +8,6 @@ module.exports = {
     truncate: function (str, len) {
         if (str.length > len && str.length > 0) {
             let new_str = str + ' '
-            new_str = str + ' '
             new_str = str.substr(0, len)
             new_str = str.substr(0, new_str.lastIndexOf(' '))
             new_str = new_str.length > 0 ? new_str : str.substr(0, len)
@@ -25,7 +24,7 @@ module.exports = {
                 return `<a href="/stories/edit/${storyId}" class = "btn-floating
                 halfway-fab blue"><i class = "fas fa-edit fa-small"></i></a> `
             } else {
-                return `<a href = "/stories/edit/${stroyId}"><i class= "fas fa-edit"></i></a>`
+                return `<a href = "/stories/edit/${storyId}"><i class= "fas fa-edit"></i></a>`
             }
         } else {
             return ''
--- routes/stories.js
+++ routes/stories.js
@@ -48,6 +48,32 @@ router.get('/', ensureAuth, async (req, res) => {
   }
 })
 
+
+
+
+// @desc    Show single story
+// @route   GET /stories/:id
+router.get('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).populate('user').lean()
+
+    if (!story) {
+      return res.render('error/404')
+    }
+
+    if (story.user._id != req.user.id && story.status == 'private') {
+      res.render('error/404')
+    } else {
+      res.render('stories/show', {
+        story,
+      })
+    }
+  } catch (err) {
+    console.error(err)
+    res.render('error/404')
+  }
+})
+
 // @desc    Show edit page
 // @route   GET /stories/edit/:id
 router.get('/edit/:id', ensureAuth, async (req, res) => {
@@ -75,7 +101,7 @@ router.get('/edit/:id', ensureAuth, async (req, res) => {
 
 
 // @desc  Update story 
-// @route PUT /stories/add
+// @route PUT /stories/:id
 router.put('/:id', ensureAuth, async (req, res) => {
   try {
     const story = await Story.findById({
@@ -104,5 +130,40 @@ router.put('/:id', ensureAuth, async (req, res) => {
     return res.render('error/500')
   }
 })
+
+// @desc  Delete story 
+// @route DELETE /stories/:id
+router.delete('/:id', ensureAuth, async (req, res) => {
+  try {
+       await Story.remove({ _id: req.params.id})
+       res.redirect('/dashboard')
+  } catch (err) {
+    console.error(err)
+    return res.render('error/500')
+  }
+})
+
+
+
+// @desc  User stories
+// @route GET /stories/user/:id
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+     const stories = await Story.find({
+       user : req.params.userId,
+       status : 'public'
+     })
+     .populate('user')
+     .lean()
+
+     res.render('stories/index', {
+       stories
+     })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 
 module.exports = router
